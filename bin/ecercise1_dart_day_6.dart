d8ff3128279e7748e6e422c2dd1027582da10aa4
abobakerbawazir <134743480+abobakerbawazir@users.noreply.github.com>
2023-09-12 21:50:39 +0300

    Add Q6.pdf

--- bin/ecercise1_dart_day_6.dart
+++ bin/ecercise1_dart_day_6.dart
@@ -3,3 +3,21 @@
 // void main(List<String> arguments) {
 //   print('Hello world: ${ecercise1_dart_day_6.calculate()}!');
 // }
+String get usecase => "Am I am useless? true";
+
+//enum way 
+
+enum OperatingSystem {
+  uselessSingleton(true);
+
+  const OperatingSystem(this.canBuildForIos);
+  final bool canBuildForIos;
+
+        String get usecase => "Am I am useless? $canBuildForIos";
+}
+
+//same result
+void main(){
+  print(usecase);
+print(OperatingSystem.uselessSingleton.usecase);
+}
