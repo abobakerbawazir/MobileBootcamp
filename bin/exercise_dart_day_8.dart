8ed318154632b66de3f34f4b9095eab1dd35a68a
abobakerbawazir <134743480+abobakerbawazir@users.noreply.github.com>
2023-09-13 20:59:12 +0300

    add all files

---
+++ bin/exercise_dart_day_8.dart
@@ -0,0 +1,5 @@
+// import 'package:exercise_dart_day_8/exercise_dart_day_8.dart' as exercise_dart_day_8;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${exercise_dart_day_8.calculate()}!');
+// }
---
+++ test/exercise_dart_day_8_test.dart
@@ -0,0 +1,8 @@
+// import 'package:exercise_dart_day_8/exercise_dart_day_8.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
