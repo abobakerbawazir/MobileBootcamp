0d963a5cf243a7e5b01d58ec50a47929169d55ff
abobakerbawazir <134743480+abobakerbawazir@users.noreply.github.com>
2023-11-20 11:21:36 +0300

    addd

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/core/views/screens/online_roucts_creen.dart';
+import 'package:flutter_application_2/day_58_storeage_get/HomeGetScreen.dart';
+import 'package:flutter_application_2/day_floor/Database/db_helper.dart';
+import 'package:flutter_application_2/day_floor/Entites/department.dart';
+import 'package:flutter_application_2/day_floor/Entites/student.dart';
+import 'package:flutter_application_2/day_floor/Views/Screens/DepHomePage.dart';
+import 'package:flutter_application_2/day_floor/Views/Screens/HomePageScreen.dart';
+import 'package:flutter_application_2/day_floor/Views/Screens/StudentHomePage.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await DBHelper.initDatabase();
+//   runApp(MyApp());
+// }
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+ 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: OnlineProductsScreen()
-    );
-  }
-}
+      title: 'Material App', home: StudentHomePage(),
+      // Scaffold(
+      //     floatingActionButton: FloatingActionButton(
+      //       onPressed: () async {
+      //         int x = await DBHelper.database.studentDao.addStudent(Student(
+      //             name: 'ahmed',
+      //             email: 'Ahmed@gmail.com',
+      //             phoneNo: '775630183',
+      //             departmentId: 1));
+      //         print(x);
+      //         setState(() {
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //         });
+      //       },
+      //     ),
+      //     appBar: AppBar(
+      //       title: const Text('Material App Bar'),
+      //     ),
+      //     body: SingleChildScrollView(
+      //       child: Column(
+      //         children: [
+      //           SizedBox(height: MediaQuery.of(context).size.height/2,
+      //             child: FutureBuilder(
+      //               future: DBHelper.database.studentDao.getAllStudents(),
+      //               builder: (context, AsyncSnapshot<List<Student>> snapshot) {
+      //                 if (snapshot.hasData) {
+      //                   return snapshot.data!.isEmpty
+      //                       ? Center(child: Text("Empty"))
+      //                       : ListView.builder(
+      //                           itemCount: snapshot.data!.length,
+      //                           itemBuilder: (context, index) {
+      //                             return Text(snapshot.data![index].name!);
+      //                           },
+      //                         );
+      //                 } else {
+      //                   return Text("data is no");
+      //                 }
+      //               },
+      //             ),
+      //           ),
+      //           SizedBox(height: MediaQuery.of(context).size.height/2,
+      //             child: FutureBuilder(
+      //               future: DBHelper.database.departmentDao.getAllDepartments(),
+      //               builder: (context, AsyncSnapshot<List<Department>> snapshot) {
+      //                 if (snapshot.hasData) {
+      //                   return snapshot.data!.isEmpty
+      //                       ? Center(child: Text("Empty"))
+      //                       : ListView.builder(
+      //                           itemCount: snapshot.data!.length,
+      //                           itemBuilder: (context, index) {
+      //                             return Text(snapshot.data![index].name!);
+      //                           },
+      //                         );
+      //                 } else {
+      //                   return Text("data is no");
+      //                 }
+      //               },
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     )),
     );
   }
 }
